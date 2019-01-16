0962ea8e792503d5f348219e3c62a4fc9691e4fa
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2019-01-16 02:09:50 +0100

    Split Example file and move into Examples directory

--- Examples/BasicExample.cs
+++ Examples/BasicExample.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using ModSettings;
+﻿using ModSettings;
 using UnityEngine;
 using static CustomExperienceModeManager;
 
@@ -57,72 +56,21 @@ namespace ModSettingsExample {
 		}
 	}
 
-	internal class VisibilityExample : ModSettingsBase {
-		[Name("Number of visible custom settings")]
-		[Description("... in the \"Custom GUI Examples\" section below")]
-		[Slider(0, 7)]
-		public int visibleSettingsCount = 7;
-
-		/*
-		 * This method is called whenever a field in this object is changed.
-		 * The method is not marked abstract, so you don't have to override it if you don't need it.
-		 */
-		protected override void OnChange(FieldInfo field, object oldValue, object newValue) {
-			// We already know there's just one field, but let's be pretend we don't
-			if (field.Name == "visibleSettingsCount") {
-				int visible = (int) newValue;
-				Test.SetFieldsVisible(visible);
-			}
-		}
-	}
-
 	internal enum ExampleEnum {
 		Utopia, Something_in_between, DystopianNightmare
 	}
 
-	internal static class Test {
+	internal static class BasicExample {
 
-		private static readonly ExampleSettings customSettings = new ExampleSettings();
-		private static readonly VisibilityExample onChangeExample = new VisibilityExample();
-		private static readonly PaddingSettings padding = new PaddingSettings();
+		internal static readonly ExampleSettings settings = new ExampleSettings();
 
 		public static void OnLoad() {
-			customSettings.AddToCustomModeMenu(Position.AboveAll);
+			// We can add this to the custom mode menu, somewhere above, below, or between Hinterland's settings
+			settings.AddToCustomModeMenu(Position.AboveAll);
 
-			onChangeExample.AddToModSettings("Custom Settings Example", MenuType.MainMenuOnly);
-			customSettings.AddToModSettings("Custom Settings Example");  // No 2nd argument -> visible in main menu and in-game
-
-			// Just to show off the scroll bars in the mod settings panel
-			padding.AddToModSettings("Custom Settings Example", MenuType.MainMenuOnly);
+			// And we can add them to the new mod settings menu
+			settings.AddToModSettings("Custom Settings Example", MenuType.InGameOnly); // or MenuType.MainMenuOnly, or MenuType.Both
 		}
-
-		internal static void SetFieldsVisible(int visibleFields) {
-			FieldInfo[] fields = customSettings.GetType().GetFields();
-			for (int i = 0; i < fields.Length; ++i) {
-				bool shouldBeVisible = i < visibleFields;
-				customSettings.SetFieldVisible(fields[i], shouldBeVisible);
-			}
-		}
-	}
-
-	// Just to show off the scroll bars in the mod settings panel. Seriously, ignore this
-	internal class PaddingSettings : ModSettingsBase {
-		[Section("Padding")]
-
-		[Name("P")]
-		public bool p = true;
-		[Name("A")]
-		public bool a = true;
-		[Name("D")]
-		public bool d0 = true;
-		[Name("D")]
-		public bool d1 = true;
-		[Name("I")]
-		public bool i = true;
-		[Name("N")]
-		public bool n = true;
-		[Name("G")]
-		public bool g = true;
 	}
 #endif
 }
---
+++ Examples/VisibilityExample.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection;
+using ModSettings;
+
+namespace ModSettingsExample {
+#if DEBUG // Change build profile to Debug to enable or Release to disable this example
+
+	/*
+	 * Example for how the onChange method and the field visibility methods work.
+	 * Adds a slider that lets you change how many settings from BasicExample are visible.
+	 */
+	internal class VisibilityExampleSettings : ModSettingsBase {
+		[Section("Visibility Example")]
+		[Name("Number of visible custom settings")]
+		[Description("... in the \"Custom GUI Examples\" section below")]
+		[Slider(0, 7)]
+		public int visibleSettingsCount = 7;
+
+		/*
+		 * This method is called whenever a field in this object is changed.
+		 * The method is not marked abstract, so you don't have to override it if you don't need it.
+		 */
+		protected override void OnChange(FieldInfo field, object oldValue, object newValue) {
+			// We already know there's just one field, but let's be pretend we don't
+			if (field.Name == nameof(visibleSettingsCount)) {
+				int visible = (int) newValue;
+				VisibilityExample.SetFieldsVisible(visible);
+			}
+		}
+	}
+
+	internal static class VisibilityExample {
+
+		private static readonly VisibilityExampleSettings onChangeExample = new VisibilityExampleSettings();
+		private static readonly ExampleSettings basicExampleSettings = new ExampleSettings();
+		private static readonly PaddingSettings padding = new PaddingSettings();
+
+		public static void OnLoad() {
+			// Let's have these in the main menu only
+			onChangeExample.AddToModSettings("Custom Settings Example", MenuType.MainMenuOnly);
+			basicExampleSettings.AddToModSettings("Custom Settings Example", MenuType.MainMenuOnly);
+
+			// Just to show off the scroll bars in the mod settings panel
+			padding.AddToModSettings("Custom Settings Example", MenuType.MainMenuOnly);
+		}
+
+		internal static void SetFieldsVisible(int visibleFields) {
+			FieldInfo[] fields = basicExampleSettings.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public);
+
+			for (int i = 0; i < fields.Length; ++i) {
+				bool shouldBeVisible = i < visibleFields;
+				basicExampleSettings.SetFieldVisible(fields[i], shouldBeVisible);
+			}
+		}
+	}
+
+	// Just to show off the scroll bars in the mod settings panel. Seriously, ignore this
+	internal class PaddingSettings : ModSettingsBase {
+		[Section("Padding")]
+
+		[Name("P")]
+		public bool p = true;
+		[Name("A")]
+		public bool a = true;
+		[Name("D")]
+		public bool d0 = true;
+		[Name("D")]
+		public bool d1 = true;
+		[Name("I")]
+		public bool i = true;
+		[Name("N")]
+		public bool n = true;
+		[Name("G")]
+		public bool g = true;
+	}
+#endif
+}
